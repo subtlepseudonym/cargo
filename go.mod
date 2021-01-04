1d18339fbc66825f10e1fe7e4456a8617d251023
Connor Demille <subtlepseudonym@gmail.com>
2021-01-04 17:03:35 -0500

    Draw orbiting bodies, first pass

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/subtlepseudonym/cargo
 
 go 1.15
 
-require github.com/hajimehoshi/ebiten/v2 v2.0.2
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.0.2
+	github.com/pbnjay/pixfont v0.0.0-20200714042608-33b744692567
+)
--- orbit.go
+++ orbit.go
@@ -1,70 +1,118 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/pbnjay/pixfont"
 )
 
 const (
-	screenWidth  = 320
-	screenHeight = 240
+	screenWidth  = 400
+	screenHeight = 400
 )
 
-type circle struct {
-	x   float64
-	y   float64
+var (
+	useStep     = false
+	step        = false
+	Orange      = color.RGBA{255, 165, 0, 255}
+	DeepSkyBlue = color.RGBA{0, 191, 255, 255}
+	FireBrick   = color.RGBA{178, 34, 34, 255}
+)
+
+type Body struct {
 	r   float64
 	clr color.Color
+
+	x  float64
+	y  float64
+	dx float64
+	dy float64
+
+	mass float64
 }
 
-func NewCircle(x, y, radius float64, clr color.Color) *circle {
-	return &circle{
-		x:   x,
-		y:   y,
-		r:   radius,
-		clr: clr,
-	}
-}
-
-func (c *circle) ColorModel() color.Model {
+func (b *Body) ColorModel() color.Model {
 	return color.RGBAModel
 }
 
-func (c *circle) Bounds() image.Rectangle {
-	minX, minY := int(c.x-c.r), int(c.y-c.r)
-	maxX, maxY := int(c.x+c.r), int(c.y+c.r)
-	return image.Rectangle{
-		Min: image.Pt(minX, minY),
-		Max: image.Pt(maxX, maxY),
-	}
+func (b *Body) Bounds() image.Rectangle {
+	return image.Rect(int(b.x-b.r), int(b.y-b.r), int(b.x+b.r), int(b.y+b.r))
 }
 
-func (c *circle) At(x, y int) color.Color {
-	xx, yy := float64(x)-c.x, float64(y)-c.y
-	if xx*xx+yy*yy < c.r*c.r {
-		return c.clr
+func (b *Body) At(x, y int) color.Color {
+	xx, yy := float64(x-int(b.x))+0.5, float64(y-int(b.y))+0.5 // FIXME
+	if xx*xx+yy*yy < b.r*b.r {
+		return b.clr
 	}
 	return color.Alpha{0}
 }
 
 type Game struct {
-	sprites []*circle
+	sun     *Body
+	planets []*Body
 }
 
 func (g *Game) Update() error {
+	if useStep {
+		if !ebiten.IsKeyPressed(ebiten.KeySpace) {
+			if step {
+				step = false
+			}
+			return nil
+		} else if step {
+			return nil
+		}
+		step = true
+	}
+
+	for _, planet := range g.planets {
+		gravityX, gravityY := gravity(g.sun, planet)
+		planet.dx = planet.dx + gravityX
+		planet.dy = planet.dy + gravityY
+
+		planet.x = planet.x + planet.dx
+		planet.y = planet.y + planet.dy
+	}
+
 	return nil
 }
 
+func gravity(sun, planet *Body) (float64, float64) {
+	distX := sun.x - planet.x
+	distY := sun.y - planet.y
+	distSq := math.Pow(distX, 2) + math.Pow(distY, 2)
+	dist := math.Sqrt(distSq)
+
+	f := (sun.mass * planet.mass) / distSq
+	fx := (distX / dist) * f
+	fy := (distY / dist) * f
+
+	return fx, fy
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	for _, sprite := range g.sprites {
-		img := ebiten.NewImageFromImage(sprite)
-		geom := ebiten.GeoM{}
-		geom.Translate(sprite.x, sprite.y)
-		screen.DrawImage(img, &ebiten.DrawImageOptions{GeoM: geom})
+	drawBody(screen, g.sun)
+	for i, planet := range g.planets {
+		drawBody(screen, planet)
+		pos := fmt.Sprintf("%.2f, %.2f", planet.x, planet.y)
+		pixfont.DrawString(screen, 0, i*10, pos, color.White)
 	}
+}
+
+func drawBody(screen *ebiten.Image, body *Body) {
+	img := ebiten.NewImageFromImage(body)
+	geom := ebiten.GeoM{}
+	geom.Translate(body.x-body.r, body.y-body.r)
+
+	screen.DrawImage(img, &ebiten.DrawImageOptions{
+		GeoM:   geom,
+		Filter: ebiten.FilterNearest,
+	})
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -72,13 +120,39 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
+	sol := &Body{
+		x:    screenWidth / 2,
+		y:    screenHeight / 2,
+		r:    40,
+		clr:  Orange,
+		mass: 10,
+	}
+	earth := &Body{
+		x:    screenWidth / 2,
+		y:    80,
+		r:    10,
+		clr:  DeepSkyBlue,
+		dx:   -0.28,
+		mass: 1,
+	}
+	mars := &Body{
+		x:    screenWidth / 2,
+		y:    20,
+		r:    8,
+		clr:  FireBrick,
+		dx:   -0.21,
+		mass: 0.8,
+	}
+
 	game := &Game{
-		sprites: []*circle{
-			NewCircle(10, 10, 5, color.RGBA{255, 0, 0, 255}),
+		sun: sol,
+		planets: []*Body{
+			earth,
+			mars,
 		},
 	}
 
-	ebiten.SetWindowSize(400, 300)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("cargo")
 
 	log.Fatal(ebiten.RunGame(game))
